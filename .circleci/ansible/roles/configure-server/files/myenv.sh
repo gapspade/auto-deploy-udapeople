9be5ff5389c69d99f94c47bc09d22cdd8681741e
gapspade <gideonade01@gmail.com>
2023-03-27 11:05:16 +0100

    fix: remove main files

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++
@@ -1,9 +0,0 @@
-TYPEORM_CONNECTION=postgres
-TYPEORM_MIGRATIONS_DIR=./src/migrations
-TYPEORM_MIGRATIONS=./src/migrations/*.ts
-TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-TYPEORM_HOST=udapeople-prod.csiejwflob3o.us-east-1.rds.amazonaws.com
-TYPEORM_PORT=5432
-TYPEORM_USERNAME=postgres
-TYPEORM_PASSWORD=postgres
-TYPEORM_DATABASE=postgres
